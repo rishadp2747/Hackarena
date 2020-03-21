81b05bf97689c2c65a9725652dab584fe0366fc5
Ayisha <ayishanavas.e>
2020-03-21 10:44:08 -0700

    commit performance

---
+++ database/migrations/2020_03_21_145309_create_performances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerformancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performances', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('challenge_id');
+            $table->string('user_id');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performances');
+    }
+}

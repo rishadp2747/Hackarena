69794f7d2a5783a8379cbb4b8dc0b5a851517b98
Rishad P <rishadp2747@gmail.com>
2020-03-30 09:48:18 +0000

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     public function index()
     {
 
-        $roles = DB::table('challenges')->select('challenge_name','challenge_route')->get();
+        $roles = DB::table('challenges')->select('challenge_name','challenge_route','id')->get(); 
         $score  = Auth::user()->total_score;
 
         return view('pages.dashboard',['title' => 'Dashboard', 'score' => $score, 'challenges' => $roles]);

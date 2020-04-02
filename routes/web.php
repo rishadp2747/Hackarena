661b380fb0fddbfc463ac6899cf206bca683d7d3
Rishad P <rishadp2747@gmail.com>
2020-04-02 12:51:00 +0530

    Route Upadate with perfomance

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\Middleware\Authenticate;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,12 @@ Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('challenge','ChallengesController');
+
+Route::get('challenges/findme/{id}', 'ChallengesController@findme')->name('findme')->middleware('auth');
+
+Route::post('perfomance/{id}','PerfomanceController@store')->name('storePerfomance')->middleware('auth');
+
+//to redirect back when perfomance hit with a get request
+Route::get('perfomance/{id}', function () {
+    return redirect()->back();
+});
